79f29cf4fa3072824980e5680a9270d58c4b8262
Johnwick4488 <115544081+Johnwick4488@users.noreply.github.com>
2023-07-31 10:58:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,3 +11,17 @@ module "s3_bucket" {
     enabled = true
   }
 }
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-santosh-000022452"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+}
